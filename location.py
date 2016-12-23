24e6546bca49cfc3c57512c6404c9e2ea5f9e889
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-12-23 19:40:24 +0100

    Rewrite to use json templates. WIP.

--- location.py
+++ location.py
@@ -1,77 +1,117 @@
-#!/usr/bin/python
-
-# template = """{% for target in targets %}
-#     $JOB/{{ target.url }} permission flag: {{ target.permission }} link to -->\
-#     None{{ target.linkto }}{% endfor %}
-
-# """
-
-# from jinja2 import Template
-# t = Template(template)
-
-# targets = {"targets": [{'url': 'camera', 'permission': 077, 'linkto':None },
-#            {'url': 'geo', 'permission': 077, 'linkto':None }]}
-
-# print t.render(targets)
-
 import os
 import stat
 import json
 
-class Location(dict):
-    def __init__(self, source=None, **kwargs):
-        super(Location, self).__init__()
 
-        if source:
-            for k, v in source.items():
+class RenderedLocation(dict):
+    """ This is basic database to propagete (render) all 
+        information per directory. It is only used by
+        workers to execute actual commands on disk or
+        remote location. It's like a rendered flatten image of 
+        all changes that will be performed.
+    """
+    def __init__(self, path, source, parent=None):
+        """ This populates generated paths with common attributes.
+        """
+        self['path'] = path
+        self['permission'] = {'group': False, 'others': False}
+        self['ownership']  = {'user' : None, 'group': 'artists'}
+        self['is_link']     = False
+
+        # Parent copies settings first if provided:
+        if parent:
+            for k, v in parent.items():
+                if k in self.keys():
+                    self[k] = v
+
+        # Then source otherrides if desired.
+        for k, v in source.items():
+            if k in self.keys():
                 self[k] = v
-        else:
-            self['root']        = '/PROD/dev'
-            self['job_name']    = "sandbox"
-            self['job_group']   = "user"
-            self['job_asset']   = "$USER"
-            self['permission']  = ['']
-            # self['']
-            self['is_link']     = False
-            self['link_target'] = None
-            self['link_root']   = None
-            self['user_dirs']   = False
-            self['names']       = []
-            self['sub_dirs']    = {}
 
+
+
+class LocationTemplate(dict):
+    """ This is tempate class reading settings per job AND per sub directory
+        of an asset / shot. It keeps track to directory structure and permissions.
+        It's main functionality is rendering templates into a list of RenderedLocation
+        objects. It's also encapsulates some on-disk activities, although they should be
+        probably moved to a specialized Device Object. 
+
+        LocationTemplate takas 'schema', which is a directory of template's dictionaries
+        provided by Job class and a 'obj', which is a name allowing to initilize it with 
+        selected template.
+    """
+    schema = {}
+    def __init__(self, schema, obj, **kwargs):
+        super(LocationTemplate, self).__init__(schema[obj])
+    
         for k, v in kwargs.items():
             self[k] = v
 
-    def is_link(self):
-        return self['is_link']
 
-    def render(self, _root=None, recursive=True):
+    def expand_path_template(self, template=None):
+        """ 
+        """
+        from os.path import join
+
+        if not template:
+            if  'path_template' in self.keys():
+                template = self['path_template']
+            else:
+                raise Exception, "No template found at %s" % str(self)
+
+        items = template.split("/")
+        dirs = []
+        for item in items:
+            if item.startswith("@"):
+                assert item[1:] in self.keys()
+                item = self[item[1:]]
+            dirs += [item]
+            path = join(*dirs)
+        return path
+
+    def render(self, _root=None, recursive=True, parent=None):
         """ Returns a list of path created by this object along with
             possible subdirectories.
         """
-        paths = []
         from os.path import join, expandvars
-        # if self._names: else ""
 
+        renders = []
+        # If root wasn't provided take it from self or
+        # regenerate it with path_template if avaible.
         if not _root:
-            root = self['root']
-            root = expandvars(join(root, self['job_name'], \
-                self['job_group'], self['job_asset']))
+            if "path_template" in self.keys():
+                root = self.expand_path_template(self['path_template'])
+            else:
+                root = self['root']
+                # Expands env vars and use default structure.
+                root = expandvars(join(root, self['job_name'], \
+                    self['job_group'], self['job_asset']))
         else:
             root = _root 
 
         for name in self['names']:
             path = expandvars(join(root, name))
-            paths += [path]
+            details = RenderedLocation(path, self, parent)
+            renders += [details]
 
         if not recursive:
             return paths
 
         for subpath in self['sub_dirs']:
-            assert subpath in globals()
-            obj = globals()[subpath]()
-            paths += obj.render(path)
-        return paths
+            # Although the template was specified in a sub_dir
+            # its definition is not in a path, so we omitt it.
+            if not subpath in self.schema.keys():
+                continue
+
+            location = LocationTemplate(self.schema, subpath)
+            _paths = location.render(path, parent=self)
+            for p in _paths:
+                if p not in renders:
+                    renders += [p]
+
+        return renders
 
     def __repr__(self):
         """Pretty-like print with json rastezier."""
@@ -130,37 +170,77 @@ class Location(dict):
         os.chown(path, uid, gid)
 
 
-class Camera(Location):
-    def __init__(self, source=None, **kwargs):
-        super(Camera, self).__init__(source, **kwargs)
-        if not source:
-            self['names']      = ["camera"]
-            self['sub_dirs']   = {'CameraFormats':""}
+    def load_schemas(self, path, schema={}):
+        """Load *.json files defining Location objects. 
+        """
+        from glob import glob
+        location = os.path.join(path, "*.json")
+        files    = glob(location)
 
-class CameraFormats(Location):
-    def __init__(self, source=None, **kwargs):
-        super(CameraFormats, self).__init__(source, **kwargs)
-        self['names'] = ['abc', 'json', 'chan']
-
-class Render(Location):
-    def __init__(self, source=None, **kwargs):
-        super(Render, self).__init__(source, **kwargs)
-        self['names'] = ['render']
-        self['user_dirs'] = True
-        self['sub_dirs']  = {'RenderFormats': ""}
-
-class RenderFormats(Location):
-    def __init__(self, source=None, **kwargs):
-        super(RenderFormats, self).__init__(source, **kwargs)
-        self['names'] = ['mantra', 'nuke', 'afx']
+ 
+        for file in files:
+            with open(file) as file_object:
+                obj  = json.load(file_object)
+                name = os.path.split(file)[1]
+                name = os.path.splitext(name)[0]
+                schema[name] = obj
+        return schema
 
 
 
+class Job(LocationTemplate):
+    """ Hopfuly the only specialization of LocationTemplate class, 
+        which provides functionality only for parent 'job' diretory.
+        Main purpos of this class is to find and load all templates found
+        in a paths (JOBB_PATH envvar.) It should also select in future
+        Device Driver to operate on disk / remote server.
 
-# camera = Camera()
-# # print camera.is_link()
-# print camera.render(recursive=True)
-# print Render().render()
-# print Render()
+    """
+    def __init__(self, jobb_path='JOBB_PATH', **kwargs):
+        """ Initialize job by looking through JOBB_PATH locations and loading
+            schema files from there. The later path in JOBB_PATH will override
+            the former schames. 
+        """
+        from os.path import join
+        schema_locations = os.getenv(jobb_path, "./")
+        schema_locations = schema_locations.split(":")
+
+        for directory in schema_locations:
+            schemas = self.load_schemas(join(directory, "schemas"))
+            for k, v in schemas.items():
+                self.schema[k] = v
+        super(Job, self).__init__(self.schema, "job", **kwargs)
+
+        # Asset diretory, as an exception, has no name
+        self['names'] = [""]
+
+    def make(self):
+        """ TODO: This is only fo testing purposes.
+        """
+
+        paths_to_create = self.render()
+        for details in paths_to_create:
+            if os.path.exists(details['path']):
+                print "Path exists: %s" % details['path']
+                continue
+            try:
+                os.makedirs( details['path'])
+            except:
+                print "Couldn't make %s" %  details['path']
+
+        for details in paths_to_create:   
+
+            self.remove_write_permissions(details['path'])
+            self.add_write_permissions(details['path'], **details['permission'])
+            self.set_ownership(details['path'], **details['ownership'])
 
 
+
+
+if __name__ == "__main__":
+
+    job = Job(job_name='sandbox', root='/tmp/dada')
+   
+    print job.render()
+    print job.make()
+   
