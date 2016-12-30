020caa69ef3f058bc7849ac43d4c779b6c8d5a76
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-12-30 16:02:24 +0100

    job can pass more options to create commands (mostly proof of concept.)

--- commands/create.py
+++ commands/create.py
@@ -4,26 +4,51 @@ class CreateJobTemplate(Base):
     """ Sub command which performs creation of disk directories
        based on schemas.
     """
+    job = None
+    def get_log_level(self):
+        """
+        """
+        import logging
+        log_level = logging.INFO
+        # This is ugly, csubcommand should extend list of options (like plugin)
+        # not rely on bin/job to provide corrent switches. 
+        if self.options['--log_level']:
+            try:
+                log_level = getattr(logging, self.options['--log_level'])
+            except:
+                pass
+        return log_level
  
-    def run(self):
+    def create_command(self):  
+        """
+        """     
         from job.cli import Job
-        from logging import DEBUG, INFO
-        
-        if self.options['create']:
-            job_name  = self.options['PROJECT']
-            job_group = self.options['TYPE']
-            job_asset = self.options['ASSET']
+        log_level = self.get_log_level()
+        job_name  = self.options['PROJECT']
+        job_group = self.options['TYPE']
+        job_asset = self.options['ASSET']
 
-            if not job_group or not job_asset:
-                job_group = job_name
-                job_asset = job_name
+        if self.options['--root']:
+            root = self.options['--root']
+        else:
+            root = '/tmp/dada'
 
-            job = Job(job_name  = job_name, 
+        if not job_group or not job_asset:
+            job_group = job_name
+            job_asset = job_name
+
+        self.job = Job(job_name  = job_name, 
                       job_group = job_group, 
                       job_asset = job_asset, 
-                      log_level = INFO, 
-                      root      = '/tmp/dada')
+                      log_level = log_level, 
+                      root      = root)
 
-            job.create()
-            job.dump_local_templates()
+        self.job.create()
+ 
+    def run(self):
+
+        if self.options['create']:
+            self.create_command()
+        if self.job and not self.options['--no_local_schema']:
+            self.job.dump_local_templates()
       
