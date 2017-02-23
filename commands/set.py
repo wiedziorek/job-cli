57a53fce124ee5eed3d7939eef0f4a73d9d23617
symek <szymon.kapeniak@gmail.com>
2017-02-24 00:01:56 +0100

    Set can read now options from a history saved in home/.job folder. This allows to use shorten cli commands among other options

--- commands/set.py
+++ commands/set.py
@@ -38,6 +38,9 @@ from .base import BaseSubCommand
 class NoJobEnvironmentBackend(Exception):
     """ Raised when JobEnvironment can't find requested backend plugin."""
 
+class SetCommandNotSuccessful(Exception):
+    """ Set command could not be performed. Leaving in current shell."""
+
 
 class JobEnvironment(object):
     """ Renders job environment gathering bits of information from
@@ -48,37 +51,25 @@ class JobEnvironment(object):
     """
     cli_options = None
     job_path    = None
-    def __init__(self, cli_options, log_level='INFO'):
+    def __init__(self, cli_options=None, log_level='INFO'):
         """ Copy all settings from cli and creates job template class from
             a description. Also initialize environment plugin, saves it as 
             a backend object to be used for something usefull.
         """
-        from os.path import join, expanduser, isdir
         from os import mkdir
+        from os.path import join, expanduser, isdir
         from job.plugin import PluginManager
         from job.logger import LoggerFactory 
+        import plugins
 
-        self.cli_options    = cli_options
-        self.job_current    = self.cli_options['PROJECT']
-        self.job_asset_type = self.cli_options['TYPE']
-        self.job_asset_name = self.cli_options['ASSET']
-        self.log_level      = log_level
-        self.logger         = LoggerFactory().get_logger("JobEnvironment", level=log_level)
-
-        if self.cli_options['--root']:
-            self.root = self.cli_options['--root']
-        else:
-            self.root = None
-
-        self.job_template = self.__create_job_template()
-        self.job_path     = self.job_template.expand_path_template()
+        self.log_level    = log_level
+        self.logger       = LoggerFactory().get_logger("JobEnvironment", level=log_level)
+        self.plg_manager  = PluginManager(log_level=log_level)
         self.package_path = join(expanduser("~"), ".job")
 
         if not isdir(self.package_path):
             self.logger.debug("No ~/.job directory. Creating it.")
             mkdir(self.package_path)
-
-        self.plg_manager = PluginManager(log_level=log_level)
 
         # TODO: Make it configurable
         environ_plugin_name = "RezEnvironment"
@@ -91,8 +82,45 @@ class JobEnvironment(object):
             self.logger.exception(message, environ_plugin_name)
             raise NoJobEnvironmentBackend(message % environ_plugin_name)
 
-        # Bind job_template to the context:
+        if cli_options:
+            self.init(cli_options)
+
+    def init(self, cli_options, history=True):
+        """ Make all steps requireing cli_options. We might saved this dict
+            in history, so not always command line is what we are after. 
+        """
+        from os.path import join, expanduser, isdir
+        from os import mkdir
+        from json import dumps
+
+        self.cli_options    = cli_options
+        self.job_current    = self.cli_options['PROJECT']
+        self.job_asset_type = self.cli_options['TYPE']
+        self.job_asset_name = self.cli_options['ASSET']
+
+        if self.cli_options['--root']:
+            self.root = self.cli_options['--root']
+        else:
+            self.root = None
+
+        self.job_template = self.__create_job_template()
+        self.job_path     = self.job_template.expand_path_template()
+
+        # Bind (init) job context tp the job_template:
         self.backend(self.job_template)
+
+        if not history or not self.job_current:
+            return
+            
+        # Save history:
+        history_folder = join(self.package_path, self.job_current)
+        if not isdir(history_folder):
+            mkdir(history_folder)
+
+        with open(join(history_folder, "job.history"), 'w') as file:
+            file.write(dumps(self.cli_options))
+        with open(join(self.package_path, "job.history"), 'w') as file:
+            file.write(dumps(self.cli_options))
 
 
     def find_job_context(self, job_template=None):
@@ -162,6 +190,44 @@ class JobEnvironment(object):
                     return
         return ok
 
+    def get_history_from_file(self, cli_options):
+        """ Finds historic cli_options to recreate JobEnvironment based on 
+            saved information (mostly job_current, job_asset_type, job_asset_name).
+
+            There are two histories suppored: 
+                    a) last set on any project: job set 
+                    b) last set to specified project: job set PROJECT [rest arg is ignored]
+
+            Parms: 
+                cli_options: current cli_options (possibly no args)
+
+            Return: 
+                retrieved from history cli_options
+        """
+        from os.path import isfile, join
+        from json import load
+
+        if not cli_options['PROJECT']:
+            history_folder = self.package_path
+        else:
+            history_folder = join(self.package_path, cli_options['PROJECT'])
+
+        history_file = join(history_folder, "job.history")
+
+        if not isfile(history_file):
+            self.logger.debug("No history file: %s", history_file)
+            return None
+
+        with open(history_file) as file:
+            try:
+                cli_options = load(file)
+                self.logger.debug("Reading cli_options from file: %s", history_file)
+            except IOError, e:
+                self.logger.exception("Can't read history from %s", history_file)
+                return None
+
+        return cli_options
+
 
 class SetEnvironment(BaseSubCommand):
     """ Performs setting up of the environment per job.
@@ -175,23 +241,43 @@ class SetEnvironment(BaseSubCommand):
 
         log_level   = self.cli_options['--log-level']
         self.logger = LoggerFactory().get_logger("SetEnvironment", level=log_level)
+        job         = JobEnvironment(log_level=log_level)
 
-        job = JobEnvironment(self.cli_options, log_level=log_level)
-        ok  = job.find_job_context()
+        # Lack of PROJECT or ASSET specification triggers reading history file
+        if None in (self.cli_options['PROJECT'], self.cli_options['TYPE'], self.cli_options['ASSET']):
 
-        if not ok:
-            context_name, package_name, package_version = job.backend.context_name()
-            self.logger.warning("Not package %s found... creating it.", package_name)
-            ok = job.backend.create_context()
+            # Can't deal with both missing args and refreshing...
+            if self.cli_options['--refresh']:
+                self.logger.exception("Can't set with Refresh and missing arguments, ")
+                raise SetCommandNotSuccessful("Either remove --refresh flag or specify project type asset. ")
+
+            cli_options = job.get_history_from_file(self.cli_options)
+
+            if cli_options:
+                self.cli_options = cli_options
+            else:
+                self.logger.exception("Can't set to the job context. Try to set using: job set PROJECT TYPE ASSET --refresh ")
+                raise SetCommandNotSuccessful
+
+        # Postponed initlilization:
+        job.init(self.cli_options) 
 
         # Read additional packages from command line:
         rez_package_names = []
         if self.cli_options['--rez']:
             rez_package_names += self.cli_options['--rez']
 
-        # Some might be also added in job.options:
+        # ...some might be also added in job.options:
         if "--rez" in job.job_template.job_options:
             rez_package_names += job.job_template.job_options['--rez']
+
+        ok  = job.find_job_context()
+
+        # Either this is first set to this asset or we want to refresh its definition:
+        if not ok or self.cli_options['--refresh']:
+            context_name, package_name, package_version = job.backend.context_name()
+            self.logger.warning("Not package %s found... creating it.", package_name)
+            ok = job.backend.create_context(requires=rez_package_names, custom={'test':'test'})
 
         if ok:
             try:
@@ -199,7 +285,7 @@ class SetEnvironment(BaseSubCommand):
                 job.backend.execute_context(rez_package_names)
             except Exception, e:
                 self.logger.exception("Can't set to the job context, ", e)
-                raise Exception
+                raise SetCommandNotSuccessful(e)
 
         else:
             self.logger.info("Can't set to job. Exiting.")
